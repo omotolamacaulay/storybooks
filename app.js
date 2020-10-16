7dca734f36838302ece9aaeddd47cdc80c7a40d1
Omotola Macaulay <tolamacaulay@gmail.com>
2020-10-16 17:56:42 +0100

    handle google auth

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+
+
+// Load Routes
+const auth = require('./routes/auth');
 
 const app = express()
+
+// Passport Config
+require('./config/passport')(passport);
 
 app.get('/', (req, res) => {
   res.send('It Works!');
 });
+
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://Tmac:ragnar33@storybooks-dev.vijtk.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  googleClientID: '89307082186-v8ugfp2hqa7dukqtfg17bvhnomqk8o63.apps.googleusercontent.com',
+  googleClientSecret: 'mTIkYCyvoerk_RZ-lWm_EYrx'
+}
